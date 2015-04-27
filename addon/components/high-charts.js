747f27d0b67d09d33eb9aa2aeb811e9bf4d6210c
Kevin Jhangiani <kevin@soxhub.com>
2015-04-27 22:54:21 +0000

    copied initial source files from ember-highcharts as a base to update the addon code

---
+++ addon/components/high-charts.js
@@ -0,0 +1,85 @@
+import Ember from 'ember';
+import { setDefaultHighChartOptions } from '../utils/option-loader';
+
+const {
+	computed,
+	get,
+	set,
+	merge,
+	on,
+	observer,
+	run
+} = Ember;
+
+export default Ember.Component.extend({
+	classNames: ['highcharts-wrapper'],
+	content: undefined,
+	mode:    undefined,
+	chartOptions: undefined,
+	chart: null,
+	theme: undefined,
+	callback: undefined,
+
+	buildOptions: computed('chartOptions', 'content.@each.isLoaded', function() {
+		let chartOptions = Ember.$.extend(true, {}, get(this, 'theme'), get(this, 'chartOptions'));
+		let chartContent = get(this, 'content.length') ? get(this, 'content') : [{
+			id    : 'noData',
+			data  : 0,
+			color : '#aaaaaa'
+		}];
+
+		let defaults = { series: chartContent };
+
+		return merge(defaults, chartOptions);
+	}),
+
+	contentDidChange: observer('content.@each.isLoaded', function() {
+		if (!(get(this, 'content') && get(this, 'chart'))) {
+			return;
+		}
+
+		let chart  = get(this, 'chart');
+		let noData = chart.get('noData');
+
+		if (noData != null) {
+			noData.remove();
+		}
+
+		return get(this, 'content').forEach((series, idx) => {
+			if (chart.series[idx]) {
+				return chart.series[idx].setData(series.data);
+			} else {
+				return chart.addSeries(series);
+			}
+		});
+	}),
+
+	drawAfterRender() {
+		run.scheduleOnce('afterRender', this, 'draw');
+	},
+
+	draw() {
+		let completeChartOptions = [ get(this, 'buildOptions'), get(this, 'callback') ];
+		let mode  = get(this, 'mode');
+
+		if (typeof mode === 'string' && !!mode) {
+		  completeChartOptions.unshift(mode);
+		}
+
+		let $element = this.$();
+		window.console.log(completeChartOptions);
+		let chart    = $element.highcharts.apply($element, completeChartOptions).highcharts();
+
+		set(this, 'chart', chart);
+	},
+
+	_renderChart: on('didInsertElement', function() {
+		this.drawAfterRender();
+		setDefaultHighChartOptions(this.container);
+	}),
+
+	_destroyChart: on('willDestroyElement', function() {
+		this._super();
+		get(this, 'chart').destroy();
+	})
+});
---
+++ addon/utils/option-loader.js
@@ -0,0 +1,31 @@
+var localConfig = null;
+
+export function setDefaultHighChartOptions(container) {
+	if (!localConfig) {
+		// use options defined in highcharts-config if they exist in the container
+		var localConfigBuilder = container.lookup('highcharts-config:application');
+		if (localConfigBuilder) {
+			localConfig = localConfigBuilder(defaultOptions);
+		} else {
+			localConfig = defaultOptions;
+		}
+	}
+
+	Highcharts.setOptions(localConfig);
+}
+
+var defaultOptions = {
+	plotOptions: {
+		series: {
+			shadow: false
+		}
+	},
+
+	global: {
+		timezoneOffset: new Date().getTimezoneOffset()
+	},
+
+	credits: {
+		enabled: false
+	}
+};
